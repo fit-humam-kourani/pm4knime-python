8b34c72943308798dd6c0f6a66b9d5042cf492a9
fit-humam-kourani <humam.kourani@fit.fraunhofer.de>
2024-02-28 20:10:06 +0100

    icon added for inductive miner

--- org.pm4knime.java/src/org/pm4knime/node/discovery/dfgminer/knimeTable/InductiveMinerDFGTableNodeFactory.java
+++ org.pm4knime.java/src/org/pm4knime/node/discovery/dfgminer/knimeTable/InductiveMinerDFGTableNodeFactory.java
@@ -1,9 +1,5 @@
 package org.pm4knime.node.discovery.dfgminer.knimeTable;
 
-import org.knime.core.node.NodeDialogPane;
-import org.knime.core.node.NodeFactory;
-import org.knime.core.node.NodeView;
-import org.knime.core.node.NodeFactory.NodeType;
 import org.knime.core.node.wizard.WizardNodeFactoryExtension;
 import org.knime.core.webui.node.impl.WebUINodeConfiguration;
 import org.knime.core.webui.node.impl.WebUINodeFactory;
@@ -20,7 +16,7 @@ public final class InductiveMinerDFGTableNodeFactory extends WebUINodeFactory<In
 
     private static final WebUINodeConfiguration CONFIG = WebUINodeConfiguration.builder()
 			.name("Inductive Miner (DFG)")
-			.icon("../category-discovery.png")
+			.icon("../../category-discovery.png")
 			.shortDescription("This node implements the second step of the Inductive Miner to discover a process tree from a directly-follows graph.")
 			.fullDescription("This node  is used to discover a process tree from a directly-follows graph (DFG).\r\n"
 					+ "        A process tree is a block-structured process model where the (inner) nodes are operators (such as sequence, choice, parallel, and loop) and the leaves are activities. \r\n"
