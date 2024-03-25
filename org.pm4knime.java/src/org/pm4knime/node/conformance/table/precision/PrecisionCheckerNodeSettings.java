a3c3dfb57f84045b2b8ab23f200042463ade70a3
humam.kourani <humam.kourani@fit.fraunhofer.de>
2024-03-25 16:02:03 +0100

    webUI for precision checker

---
+++ org.pm4knime.java/src/org/pm4knime/node/conformance/table/precision/PrecisionCheckerNodeSettings.java
@@ -0,0 +1,61 @@
+package org.pm4knime.node.conformance.table.precision;
+
+import org.knime.core.webui.node.dialog.defaultdialog.DefaultNodeSettings;
+import org.knime.core.webui.node.dialog.defaultdialog.layout.After;
+import org.knime.core.webui.node.dialog.defaultdialog.layout.Layout;
+import org.knime.core.webui.node.dialog.defaultdialog.layout.Section;
+import org.knime.core.webui.node.dialog.defaultdialog.widget.ChoicesProvider;
+import org.knime.core.webui.node.dialog.defaultdialog.widget.ChoicesWidget;
+import org.knime.core.webui.node.dialog.defaultdialog.widget.Widget;
+import org.processmining.plugins.multietc.sett.MultiETCSettings;
+
+@SuppressWarnings("restriction")
+public final class PrecisionCheckerNodeSettings implements DefaultNodeSettings {
+	
+	public static interface PrecisionDialogLayout {
+	    
+		
+	    @Section(title = "OrderedPresentation")
+        interface OrderedPresentation {
+	    	
+        }   
+	    
+	    @Section(title = "Algorithm")
+        @After(OrderedPresentation.class)
+        interface Algorithm {
+        }
+      
+	 }
+	
+	final static String ALIGN_1 = "1-Align Precision";
+	final static String ALIGN_ALL = "All-Align Precision";
+	final static String ALIGN_REPRE = "Representative-Align Precision";
+	final static String ETC = "ETC Precision (no invisible/duplicates allowed)";
+	
+	public static final String[] algorithmList = {MultiETCSettings.ALGORITHM.toString() , ALIGN_1,
+			ALIGN_ALL, ALIGN_REPRE, ETC};
+	 
+	public static class AlgorithmChoicesProvider implements ChoicesProvider {
+        @Override
+        public String[] choices(final DefaultNodeSettingsContext context) {
+            return algorithmList;
+        }
+    }
+
+    
+	@Widget(title = "Ordered Representation", description = "The representation of the information in the event log (ordered sequences or multi-sets). This option is enabled by default.")
+	@Layout(PrecisionDialogLayout.OrderedPresentation.class)
+	boolean m_ignore_ll = true;
+
+    @Widget(title = "Algorithm", description = "The algorithm to calculate the precision. The following options are available:\r\n"
+    		+ "			<ul>\r\n"
+    		+ "				<li>1-Align Precision.</li>\r\n"
+    		+ "				<li>All-Align Precision.</li>\r\n"
+    		+ "				<li>Representative-Align Precision.</li>\r\n"
+    		+ "				<li>ETC Precision (no invisible/duplicates allowed). </li>\r\n"
+    		+ "				</ul>")
+    @Layout(PrecisionDialogLayout.Algorithm.class)
+    @ChoicesWidget(choices = AlgorithmChoicesProvider.class)
+    String m_variant = ALIGN_1;
+
+}
