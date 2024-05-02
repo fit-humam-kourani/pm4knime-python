4970d296f466e4be99fca935986e48bb73563ce2
samadrita.saha <samadrita.saha@rwth-aachen.de>
2024-05-02 23:39:01 +0200

    webUI for petri net to image

--- org.pm4knime.java/src/org/pm4knime/node/visualizations/jsgraphviz/JSGraphVizPNNodeFactory.java
+++ org.pm4knime.java/src/org/pm4knime/node/visualizations/jsgraphviz/JSGraphVizPNNodeFactory.java
@@ -1,67 +1,49 @@
-//package org.pm4knime.node.visualizations.jsgraphviz;
-//
-//import org.knime.core.node.wizard.WizardNodeFactoryExtension;
-//import org.pm4knime.portobject.PetriNetPortObject;
-//import org.knime.core.node.NodeDialogPane;
-//import org.knime.core.node.NodeFactory;
-//import org.knime.core.node.NodeView;
-//import org.knime.core.node.port.PortType;
-//
-///**
-// * This is an example implementation of the node factory of the
-// * "JSGraphViz" node.
-// *
-// * @author 
-// */
-//public class JSGraphVizPNNodeFactory 
-//        extends NodeFactory<JSGraphVizAbstractModel> implements WizardNodeFactoryExtension<JSGraphVizAbstractModel, JSGraphVizViewRepresentation, JSGraphVizViewValue> {
-//
-//    /**
-//     * {@inheritDoc}
-//     */
-//    @Override
-//    public JSGraphVizAbstractModel createNodeModel() {
-//		// Create and return a new node model.
-//    	PortType[] IN_TYPES = {PetriNetPortObject.TYPE};
-//        return new JSGraphVizAbstractModel(IN_TYPES, "Petri Net JS View");
-//    }
-//
-//    /**
-//     * {@inheritDoc}
-//     */
-//    @Override
-//    public int getNrNodeViews() {
-//		// The number of views the node should have, in this cases there is none.
-//        return 0;
-//    }
-//
-//    /**
-//     * {@inheritDoc}
-//     */
-//    @Override
-//    public NodeView<JSGraphVizAbstractModel> createNodeView(final int viewIndex,
-//            final JSGraphVizAbstractModel nodeModel) {
-//		// We return null as this example node does not provide a view. Also see "getNrNodeViews()".
-//		return null;
-//    }
-//
-//    /**
-//     * {@inheritDoc}
-//     */
-//    @Override
-//    public boolean hasDialog() {
-//		// Indication whether the node has a dialog or not.
-//        return false;
-//    }
-//
-//    /**
-//     * {@inheritDoc}
-//     */
-//    @Override
-//    public NodeDialogPane createNodeDialogPane() {
-//		// This example node has a dialog, hence we create and return it here. Also see "hasDialog()".
-//        return null;
-//    }
-//
-//}
-//
+package org.pm4knime.node.visualizations.jsgraphviz;
+
+import org.knime.core.webui.node.impl.WebUINodeConfiguration;
+import org.knime.core.webui.node.impl.WebUINodeFactory;
+import org.pm4knime.portobject.PetriNetPortObject;
+import org.pm4knime.util.defaultnode.EmptyNodeSettings;
+import org.knime.core.node.port.PortType;
+import org.knime.core.node.port.image.ImagePortObject;
+import org.knime.core.node.wizard.WizardNodeFactoryExtension;
+
+@SuppressWarnings("restriction")
+public class JSGraphVizPNNodeFactory extends WebUINodeFactory<JSGraphVizAbstractModel> implements WizardNodeFactoryExtension<JSGraphVizAbstractModel, JSGraphVizViewRepresentation, JSGraphVizViewValue> {
+	
+	JSGraphVizAbstractModel node;
+
+	private static final WebUINodeConfiguration CONFIG = WebUINodeConfiguration.builder()
+			.name("Petri Net To Image")
+			.icon("./process.png")
+			.shortDescription("JavaScript Visualizer for Petri Nets")
+			.fullDescription("This node implements a JavaScript visualization of Petri nets.\r\n"
+					+ "    <br/>\r\n"
+					+ "    A Petri net is a directed bipartite graph that visualizes all possible traces (order of executed activities). A Petri net consists of places, transitions, and directed arcs that connect them.  \r\n"
+					+ "    Arcs can either connect places with transitions or transitions with places. Places can contain tokens. The placement of tokens defines the state of the Petri net. There is a start and a final state.\r\n"
+					+ "    The start state has only a token in the green place (initial place). The goal state has only a token in the double margin place (final place).\r\n"
+					+ "    A place is enabled if it it contains at least one token. A transition can only fire if all incoming places are enabled. \r\n"
+					+ "    After firing a transition, a token is consumed from all of its incoming places, and a token is produced in all of its outgoing places.")//
+			.modelSettingsClass(EmptyNodeSettings.class)//
+			.addInputPort("Petri Net", PetriNetPortObject.TYPE ,"a Petri net")//
+			.addOutputPort("Image", ImagePortObject.TYPE, "an SVG image")//
+			.nodeType(NodeType.Visualizer)
+			.build();
+
+	public JSGraphVizPNNodeFactory() {
+		super(CONFIG);
+	}
+
+
+	protected JSGraphVizPNNodeFactory(final WebUINodeConfiguration configuration) {
+		super(configuration);
+	}
+
+
+	@Override
+	public JSGraphVizAbstractModel createNodeModel() {
+		PortType[] IN_TYPES = {PetriNetPortObject.TYPE};
+		node = new JSGraphVizAbstractModel(IN_TYPES, "Petri Net JS View", EmptyNodeSettings.class);
+		return node;
+	}
+}
