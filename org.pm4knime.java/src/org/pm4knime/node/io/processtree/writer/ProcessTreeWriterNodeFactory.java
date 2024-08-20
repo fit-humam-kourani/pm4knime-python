234a66a48b01444d4f780ff6fe36b516dcdd0d3b
samadrita.saha <samadrita.saha@rwth-aachen.de>
2024-08-20 21:19:35 +0200

    new dialog for process tree writer

--- org.pm4knime.java/src/org/pm4knime/node/io/processtree/writer/ProcessTreeWriterNodeFactory.java
+++ org.pm4knime.java/src/org/pm4knime/node/io/processtree/writer/ProcessTreeWriterNodeFactory.java
@@ -1,72 +1,35 @@
 package org.pm4knime.node.io.processtree.writer;
 
-import java.util.Optional;
-
-import org.knime.core.node.ConfigurableNodeFactory;
-import org.knime.core.node.NodeDialogPane;
-import org.knime.core.node.NodeView;
-import org.knime.core.node.context.NodeCreationConfiguration;
-import org.knime.filehandling.core.port.FileSystemPortObject;
+import org.knime.core.webui.node.impl.WebUINodeConfiguration;
+import org.knime.core.webui.node.impl.WebUINodeFactory;
 import org.pm4knime.portobject.ProcessTreePortObject;
 
-/**
- * <code>NodeFactory</code> for the "ProcessTreeWriter" Node.
- * Export process tree into file
- *
- * @author DKF
- */
-public class ProcessTreeWriterNodeFactory 
-        extends ConfigurableNodeFactory<ProcessTreeWriterNodeModel> {
+@SuppressWarnings("restriction")
+public class ProcessTreeWriterNodeFactory extends WebUINodeFactory<ProcessTreeWriterNodeModel> {
 
+    private static final WebUINodeConfiguration CONFIG = WebUINodeConfiguration.builder() //
+        .name("ProcessTree Writer") //
+        .icon("../../write.png") //
+        .shortDescription("Export a process tree into a PTML file.") //
+        .fullDescription("""
+                <p>
+                This nodes exports a process tree into a PTML file.
+                </p>
+                """) //
+        .modelSettingsClass(ProcessTreeWriterNodeSettings.class) //
+        .addInputPort("Process Tree", ProcessTreePortObject.TYPE, "a process tree")
+        .nodeType(NodeType.Sink)
+		.build();
 
-	    private ProcessTreeWriterNodeModel model;
-	    public static final String CONNECTION_INPUT_PORT_GRP_NAME = "File System Connection";
-	    static final String INPUT_PORT_GRP_NAME = "Process Tree";
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public int getNrNodeViews() {
-        return 0;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public NodeView<ProcessTreeWriterNodeModel> createNodeView(final int viewIndex,
-            final ProcessTreeWriterNodeModel nodeModel) {
-        return null;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public boolean hasDialog() {
-        return true;
+    public ProcessTreeWriterNodeFactory() {
+        super(CONFIG);
     }
 
     @Override
-	protected Optional<PortsConfigurationBuilder> createPortsConfigBuilder() {
-		final PortsConfigurationBuilder builder = new PortsConfigurationBuilder();
-        builder.addOptionalInputPortGroup(CONNECTION_INPUT_PORT_GRP_NAME, FileSystemPortObject.TYPE);
-        builder.addFixedInputPortGroup(INPUT_PORT_GRP_NAME, ProcessTreePortObject.TYPE);
-        return Optional.of(builder);
-	}
+    public ProcessTreeWriterNodeModel createNodeModel() {
+        return new ProcessTreeWriterNodeModel(CONFIG);
+    }
 
-	@Override
-	protected ProcessTreeWriterNodeModel createNodeModel(NodeCreationConfiguration creationConfig) {
-		this.model = new ProcessTreeWriterNodeModel(creationConfig);
-		return this.model;
-	}
-	
-
-	@Override
-	protected NodeDialogPane createNodeDialogPane(NodeCreationConfiguration creationConfig) {
-		return new ProcessTreeWriterNodeDialog(creationConfig, this.model);
-	}
 
 }
 
