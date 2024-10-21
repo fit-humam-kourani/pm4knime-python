641b6ea3eb8122f903be312462885956f7c2f8c3
humam.kourani <humam.kourani@fit.fraunhofer.de>
2024-10-21 14:46:03 +0200

    adapted default discovery node to consider timestamps

--- org.pm4knime.java/src/org/pm4knime/node/discovery/defaultminer/DefaultTableMinerNodeModel.java
+++ org.pm4knime.java/src/org/pm4knime/node/discovery/defaultminer/DefaultTableMinerNodeModel.java
@@ -1,10 +1,12 @@
 package org.pm4knime.node.discovery.defaultminer;
 
-import java.util.List;
-import java.util.Map;
-
-import org.knime.base.node.preproc.sorter.SorterNodeFactory;
+import java.util.Arrays;
+import java.util.OptionalInt;
 import org.knime.core.data.DataTableSpec;
+import org.knime.core.data.StringValue;
+import org.knime.core.data.sort.BufferedDataTableSorter;
+import org.knime.core.data.sort.RowComparator;
+import org.knime.core.data.sort.RowComparator.ColumnComparatorBuilder;
 import org.knime.core.node.BufferedDataTable;
 import org.knime.core.node.ExecutionContext;
 import org.knime.core.node.InvalidSettingsException;
@@ -18,7 +20,6 @@ import org.pm4knime.node.visualizations.jsgraphviz.JSGraphVizViewRepresentation;
 import org.pm4knime.node.visualizations.jsgraphviz.JSGraphVizViewValue;
 import org.pm4knime.node.visualizations.jsgraphviz.util.WebUIJSViewNodeModel;
 import org.pm4knime.portobject.AbstractJSONPortObject;
-import org.processmining.plugins.graphviz.dot.Dot;
 
 
 public abstract class DefaultTableMinerNodeModel<S extends DefaultTableMinerSettings> extends WebUIJSViewNodeModel<S, JSGraphVizViewRepresentation, JSGraphVizViewValue> implements PortObjectHolder {
@@ -41,14 +42,51 @@ public abstract class DefaultTableMinerNodeModel<S extends DefaultTableMinerSett
 	
 	@Override
 	protected void performExecuteCreateView(PortObject[] inObjects, ExecutionContext exec) throws Exception {
-		
 		logPO = (BufferedDataTable)inObjects[0];
+        final var dts = logPO.getDataTableSpec();
+        final var sorter = new BufferedDataTableSorter(logPO, toRowComparator(dts, m_settings));
+        sorter.setSortInMemory(false);
+        final BufferedDataTable sortedTable = sorter.sort(exec);
+        logPO = sortedTable;               
 		pmPO = mine(logPO, exec);
 		
 		JSGraphVizViewRepresentation representation = getViewRepresentation();
 		representation.setJSONString(pmPO.getJSON());
 
 	}
+	
+	protected static RowComparator toRowComparator(final DataTableSpec spec, final DefaultTableMinerSettings modelSettings) {
+        final var rc = RowComparator.on(spec);
+        String[] sorting_columns = {modelSettings.t_classifier, modelSettings.time_classifier};
+        Arrays.stream(sorting_columns).forEach(column -> {
+            final var ascending = true;
+            final var alphaNum = true;
+            resolveColumnName(spec, column).ifPresentOrElse(
+                col -> rc.thenComparingColumn(col,
+                    c -> configureColumnComparatorBuilder(spec, ascending, alphaNum, col, c)),
+                () -> rc.thenComparingRowKey(
+                    k -> k.withDescendingSortOrder(!ascending).withAlphanumericComparison(alphaNum)));
+        });
+        return rc.build();
+    }
+	
+	private static ColumnComparatorBuilder configureColumnComparatorBuilder(final DataTableSpec spec, final boolean ascending, final boolean alphaNum, final int col,
+	        final ColumnComparatorBuilder c) {
+	        var compBuilder = c.withDescendingSortOrder(!ascending);
+	        if (spec.getColumnSpec(col).getType().isCompatible(StringValue.class)) {
+	            compBuilder.withAlphanumericComparison(alphaNum);
+	        }
+	        return compBuilder.withMissingsLast(false);
+	    }
+	
+	private static OptionalInt resolveColumnName(final DataTableSpec dts, final String colName) {
+	        final var idx = dts.findColumnIndex(colName);
+	        if (idx == -1) {
+	            return OptionalInt.empty();
+	        }
+	        return OptionalInt.of(idx);
+	    }
+
 
 	protected abstract AbstractJSONPortObject mine(BufferedDataTable log, final ExecutionContext exec) throws Exception; 
 	
@@ -60,7 +98,7 @@ public abstract class DefaultTableMinerNodeModel<S extends DefaultTableMinerSett
 		if (!inSpecs[0].getClass().equals(DataTableSpec.class))
 			throw new InvalidSettingsException("Input is not a valid Table!");
 		DataTableSpec logSpec = (DataTableSpec) inSpecs[0];
-		if(modelSettings.e_classifier == null || modelSettings.t_classifier == null)
+		if(modelSettings.e_classifier == null || modelSettings.t_classifier == null || modelSettings.time_classifier == null)
 			throw new InvalidSettingsException("Classifiers are not set! Please open the dialog and configure the node!");
 		return configureOutSpec(logSpec);
 	}
